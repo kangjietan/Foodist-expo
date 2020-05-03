7a1f6b3483512e3e2c93544e056ead0cc5857ac6
Kang Jie Tan <kangjietan@gmail.com>
2020-05-03 16:33:25 -0700

    Yelp API helper function andkey file

---
+++ config/config.example.js
@@ -0,0 +1,8 @@
+// Make a copy named config.js and place your keys in the file
+
+// Put your Yelp API keys here!
+const YELP_API_KEY = 'Bearer <yelp api key here>';
+
+module.exports = {
+  YELP_API_KEY,
+}
---
+++ src/api/searchYelp.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const { YELP_API_KEY } = require('../../config/config.js');
+
+const searchYelp = (params) => {
+  const cors = 'https://cors-anywhere.herokuapp.com';
+  const url = 'https://api.yelp.com/v3/businesses/search';
+
+  const options = {
+    method: 'GET',
+    headers: { Authorization: YELP_API_KEY, 'X-Requested-With': 'XMLHttpRequest' },
+    params,
+    url: `${cors}/${url}`,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(options)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+module.exports = searchYelp;
