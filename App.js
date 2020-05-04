0b96f47e1086270023d2ec61647a3924ea62e926
Kang Jie Tan <kangjietan@gmail.com>
2020-05-03 19:10:47 -0700

    Search and generate random restaurant by user location

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ class App extends Component {
     }
 
     this.rollNewRestaurant = this.rollNewRestaurant.bind(this);
+    this.rollByUserLocation = this.rollByUserLocation.bind(this);
   }
 
   componentDidMount() {
@@ -40,12 +41,25 @@ class App extends Component {
     this.setState({ randomRestaurant: restaurants[idx] });
   }
 
+  rollByUserLocation() {
+    Helpers.getUserLocation()
+      .then((coords) => {
+        const params = { latitude: coords[0], longitude: coords[1], location: 'San Francisco', term: 'food', limit: 50 };
+        searchYelp(params)
+          .then((response) => {
+            const { businesses } = response.data;
+            const randomInt = Helpers.randomInt(0, businesses.length - 1);
+            this.setState({restaurants: businesses, randomRestaurant: businesses[randomInt], render: true});
+          })
+      })
+  }
+
   render() {
     const { render, randomRestaurant } = this.state;
 
     return (
       <SafeAreaView style={styles.container}>
-        <Button title="Search By Your Location" style={styles.button} />
+        <Button title="Search By Your Location" style={styles.button} onPress={this.rollByUserLocation} />
         {render ? <RandomRestaurant restaurant={randomRestaurant} roll={this.rollNewRestaurant} /> : null}
       </SafeAreaView>
     );
--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -1,5 +1,15 @@
 module.exports = {
   randomInt: (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  getUserLocation: () => {
+    return new Promise((resolve, reject) => {
+      const showPosition = (position) => resolve([position.coords.latitude, position.coords.longitude]);
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        reject(new Error('Geolocation is not supported by this browser.'));
+      }
+    });
   }
 }
